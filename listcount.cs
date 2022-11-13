9855c59d2ab56a669b2d18a589defbd6304918bc
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-11-13 12:45:26 +0530

    Add files via upload

---
+++ listcount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class program
+{
+    public static void Main()
+    {
+
+        
+        List<int> firstlist = new List<int>();
+
+        for (int i = 1; i < 10; i++)
+        {
+            firstlist.Add(i * 2);
+        }
+
+
+        Console.WriteLine(firstlist.Count);
+    }
+}
